83da2ded2ded61ee6495e5c48df28bfcb126513d
Haohan Lin <haohan.lin@outlook.com>
2021-06-03 16:01:56 -0700

    add lecture and test code

--- test/IntList.java
+++ test/IntList.java
@@ -3,19 +3,54 @@ public class IntList {
     public IntList rest;
 
     public IntList(int f, IntList r) {
-        first = f;
-        rest = r;
+        this.first = f;
+        this.rest = r;
     }
 
     public int size() {
-        if (rest == null) {
+        if (this.rest == null) {
             return 1;
         }
-        int s  = 1 + rest.size();
-        return s;
+        return 1 + this.rest.size();
+    }
+
+    public int iterSize() {
+        IntList tempList = this;
+        int count = 0;
+        while (tempList != null) {
+            tempList = tempList.rest;
+            count ++;
+        }
+        return count;
+    }
+
+    public int get(int i) {
+        IntList tempList = this;
+        int count = 0;
+        while (tempList != null) {
+            if (count == i) {
+                return tempList.first;
+            }
+            tempList = tempList.rest;
+            count ++;
+        }
+        return 0;
+    }
+
+    public int recurGet(int i) {
+        if (i == 0) {
+            return this.first;
+        }
+        return this.rest.recurGet(i - 1);
     }
 
     public static void main(String[] args) {
+        IntList l = new IntList(1, null);
+        l = new IntList(2, l);
+        l = new IntList(3, l);
+
+        int s_pred = l.size();
+        System.out.println(s_pred);
     }
 
 }
--- test/TestIntList.java
+++ test/TestIntList.java
@@ -3,19 +3,49 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestIntList {
-
     @Test
-    public static void testSize() {
+    public void testRecurrSize() {
         IntList l = new IntList(1, null);
-        l.rest = new IntList(2, null);
-        l.rest.rest = new IntList(3, null);
-
+        l = new IntList(2, l);
+        l = new IntList(3, l);
         int s_gt = 3;
         int s_pred = l.size();
+        String errMsg = String.format("Recursion size incorrect. Should be %d but got %d", s_gt, s_pred);
+        assertEquals(errMsg, s_gt, s_pred);
+    }
 
+    @Test
+    public void testIterSize() {
+        IntList l = new IntList(1, null);
+        l = new IntList(2, l);
+        l = new IntList(3, l);
+        int s_gt = 3;
+        int s_pred = l.iterSize();
+        String errMsg = String.format("Iterative size incorrect. Should be %d but got %d", s_gt, s_pred);
+        assertEquals(errMsg, s_gt, s_pred);
+    }
 
+    @Test
+    public void testGet() {
+        IntList l = new IntList(1, null);
+        l = new IntList(2, l);
+        l = new IntList(3, l);
+        int s_gt = 3;
+        int i = 0;
+        int s_pred = l.get(i);
+        String errMsg = String.format("Get(#%d) incorrect. Should be %d but got %d", i, s_gt, s_pred);
+        assertEquals(errMsg, s_gt, s_pred);
+    }
 
-        String errMsg = String.format("Size incorrect. Should be %d but got %d", s_gt, s_pred);
+    @Test
+    public void testRecurGet() {
+        IntList l = new IntList(1, null);
+        l = new IntList(2, l);
+        l = new IntList(3, l);
+        int s_gt = 3;
+        int i = 0;
+        int s_pred = l.recurGet(i);
+        String errMsg = String.format("Get(#%d) incorrect. Should be %d but got %d", i, s_gt, s_pred);
         assertEquals(errMsg, s_gt, s_pred);
     }
 }
